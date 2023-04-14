f5400c7e11d98b47f4b4a933b88923bafd9100ac
chrismacam <christianmacam@gmail.com>
2023-04-14 12:08:11 -0300

    test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,104 +1,42 @@
 define([
     'postmonger'
-], function (
+], function(
     Postmonger
 ) {
     'use strict';
 
     var connection = new Postmonger.Session();
-    var authTokens = {};
     var payload = {};
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
-    connection.on('requestedInteraction', onRequestedInteraction);
-    connection.on('requestedTriggerEventDefinition', onRequestedTriggerEventDefinition);
-    connection.on('requestedDataSources', onRequestedDataSources);
-
     connection.on('clickedNext', save);
-   
+
+    connection.on("requestedTokens", onGetTokens);
+    function onGetTokens(tokens) {
+      console.log(tokens);
+    }
     function onRender() {
-        // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
-
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-        connection.trigger('requestInteraction');
-        connection.trigger('requestTriggerEventDefinition');
-        connection.trigger('requestDataSources');  
-
-    }
-
-    function onRequestedDataSources(dataSources){
-        console.log('*** requestedDataSources ***');
-        console.log(dataSources);
-    }
-
-    function onRequestedInteraction (interaction) {    
-        console.log('*** requestedInteraction ***');
-        console.log(interaction);
-     }
-
-     function onRequestedTriggerEventDefinition(eventDefinitionModel) {
-        console.log('*** requestedTriggerEventDefinition ***');
-        console.log(eventDefinitionModel);
+        connection.trigger("requestTokens");
     }
 
     function initialize(data) {
-        console.log(data);
         if (data) {
-            payload = data;
+            payload = data;``
+            var setcpURL = payload['arguments'].execute.inArguments[0].cloudpageURL;
+            $('#cpURL').val(setcpURL);
         }
-        
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-
-        console.log(inArguments);
-
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-                
-              
-            });
-        });
-
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
-    }
-
-    function onGetTokens(tokens) {
-        console.log(tokens);
-        authTokens = tokens;
-    }
-
-    function onGetEndpoints(endpoints) {
-        console.log(endpoints);
     }
 
     function save() {
-        var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
-
+        var cpURL = $('#cpURL').val();
         payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens
+            "subscriberKey": "{{Contact.Key}}",
+            "cloudpageURL": cpURL
         }];
-        
         payload['metaData'].isConfigured = true;
-
         console.log(payload);
         connection.trigger('updateActivity', payload);
     }
-
-
 });
